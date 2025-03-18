5efd173fc64f001a2ca29ecc2a029d330ac37d71
David Lyon <dauglyon@users.noreply.github.com>
2025-03-17 18:57:05 -0700

    Improve tree view, icon rendering. Add action button example.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def main():
-    print("Hello from cdm-schema-browser!")
+    print("Hello from cdm-tree-browser!")
 
 
 if __name__ == "__main__":
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-import { ReactWidget, ISessionContext } from '@jupyterlab/apputils';
+import { ReactWidget } from '@jupyterlab/apputils';
 import { Panel } from '@lumino/widgets';
 import React, { useMemo } from 'react';
 import {
@@ -10,12 +10,16 @@ import {
   QueryClient,
   QueryClientProvider
 } from '@tanstack/react-query';
-import faTreeSVG from '@fortawesome/fontawesome-free/svgs/solid/tree.svg';
+import faBoxesSVG from '@fortawesome/fontawesome-free/svgs/solid/boxes.svg';
 import { Tree } from 'react-arborist';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
-  IMessage,
-  IOPubMessageType
-} from '@jupyterlab/services/lib/kernel/messages';
+  faAngleDown,
+  faAngleRight,
+  faAnglesRight,
+  faRightToBracket
+} from '@fortawesome/free-solid-svg-icons';
+import { Container, IconButton, Stack, Typography } from '@mui/material';
 
 /**
  * Initialization data for the cdm-tree-browser extension.
@@ -25,33 +29,40 @@ const plugin: JupyterFrontEndPlugin<void> = {
   description:
     'A JupyterLab extension for browsing file/data trees in KBase CDM JupyterLab.',
   autoStart: true,
-  activate: async (app, sessionContext) => {
+  activate: async app => {
     console.log('JupyterLab extension cdm-tree-browser is activated!');
-    onActivate(app, sessionContext);
+    onActivate(app);
   },
   deactivate: () => {
     console.log('JupyterLab extension cdm-tree-browser was deactivated!');
   }
 };
 
-function onActivate(app: JupyterFrontEnd, sessionContext: ISessionContext) {
+function onActivate(app: JupyterFrontEnd) {
   const panel = new Panel();
   panel.id = 'cdm-tree-browser';
   panel.title.icon = {
     render: element => {
-      element.innerHTML = faTreeSVG;
+      element.innerHTML = faBoxesSVG;
+      element.setAttribute(
+        'style',
+        'align-items: center; display: flex; flex: 0 0 auto;'
+      );
+      element
+        .getElementsByTagName('svg')[0]
+        .setAttribute(
+          'style',
+          'display: block; height: auto; margin: 0 auto; width: 16px; fill: #616161;'
+        );
     }
   };
-  panel.addWidget(new TreeBrowserWidget(sessionContext));
+  panel.addWidget(new TreeBrowserWidget());
   app.shell.add(panel, 'left', { rank: 1 });
 }
 
 class TreeBrowserWidget extends ReactWidget {
-  private sessionContext: ISessionContext;
-
-  constructor(sessionContext: ISessionContext) {
+  constructor() {
     super();
-    this.sessionContext = sessionContext;
   }
 
   render(): JSX.Element {
@@ -60,103 +71,104 @@ class TreeBrowserWidget extends ReactWidget {
 
     return (
       <QueryClientProvider client={queryClient}>
-        <TreeBrowser sessionContext={this.sessionContext} />
+        <TreeBrowser />
       </QueryClientProvider>
     );
   }
 }
 
-function TreeBrowser({ sessionContext }: { sessionContext: ISessionContext }) {
+interface ITreeNode {
+  id: string;
+  name: string;
+  onAction?: (id: string) => void;
+  children?: ITreeNode[];
+}
+
+function TreeBrowser() {
   // For API calls we can use react-query (instead of rtk-query as redux is overkill)
   const query = useQuery({
     queryKey: ['namespaces'],
-    queryFn: async () =>
-      callKernel('display_namespace_viewer()', sessionContext)
+    queryFn: async () => 'someData'
   });
-  console.log('namespaces', query.status, query.data);
-  const treeData = useMemo(() => {
+  const doAction = (id: string) => {
+    console.log('action', id);
+  };
+  console.log('namespaces', query.status, query.data, query.error);
+  const treeData: ITreeNode[] = useMemo(() => {
     // perform any client-side transformations
     return [
-      { id: '1', name: 'Empty' },
-      { id: '2', name: 'Empty 2' },
+      { id: '1', name: 'Empty', icon: faAnglesRight, children: [] },
+      { id: '2', name: 'Empty 2', icon: faAnglesRight, children: [] },
       {
         id: '3',
         name: 'Some Data Source',
+        icon: faAnglesRight,
         children: [
-          { id: 'c1', name: 'Data 1' },
-          { id: 'c2', name: 'Data 2' },
-          { id: 'c3', name: 'Data 3' }
+          { id: 'c1', name: 'Data 1', onAction: doAction },
+          { id: 'c2', name: 'Data 2', onAction: doAction },
+          { id: 'c3', name: 'Data 3', onAction: doAction }
         ]
       },
       {
         id: '4',
         name: 'Another Data Source',
+        icon: faAnglesRight,
         children: [
-          { id: 'd1', name: 'Alice' },
-          { id: 'd2', name: 'Bob' },
-          { id: 'd3', name: 'Charlie' }
+          { id: 'd1', name: 'Alice', onAction: doAction },
+          { id: 'd2', name: 'Bob', onAction: doAction },
+          { id: 'd3', name: 'Charlie', onAction: doAction }
         ]
       }
     ]; // from query.data
   }, [query.data]);
   // For the tree we can use react-arborist
   return (
-    <div className="jp-TreeBrowserWidget">
-      <Tree initialData={treeData} />
-    </div>
+    <Container className="jp-TreeBrowserWidget" maxWidth="sm">
+      <Tree initialData={treeData}>
+        {({ node, style, dragHandle, tree }) => {
+          return (
+            <div
+              style={style}
+              ref={dragHandle}
+              onClick={
+                !node.isLeaf ? () => tree.get(node.id)?.toggle() : undefined
+              }
+            >
+              <Stack direction={'row'} spacing={1} alignItems={'center'}>
+                {!node.isLeaf ? (
+                  <FontAwesomeIcon
+                    fixedWidth
+                    icon={node.isOpen ? faAngleDown : faAngleRight}
+                  />
+                ) : undefined}
+                <div>
+                  <Typography variant="body1">{node.data.name}</Typography>
+                </div>
+                <div>
+                  {node.isLeaf ? (
+                    node.data.onAction ? (
+                      <IconButton
+                        size={'small'}
+                        onClick={() => node.data.onAction?.(node.id)}
+                      >
+                        <FontAwesomeIcon
+                          fontSize={'inherit'}
+                          fixedWidth
+                          icon={faRightToBracket}
+                        />
+                      </IconButton>
+                    ) : undefined
+                  ) : undefined}
+                </div>
+              </Stack>
+            </div>
+          );
+        }}
+      </Tree>
+    </Container>
   );
 }
 
-const callKernel = async (
-  code: string,
-  sessionContext: ISessionContext,
-  timeout = 1000
-) => {
-  if (!sessionContext.session?.kernel) {
-    throw new Error('No kernel available');
-  }
-
-  const future = sessionContext.session.kernel.requestExecute({
-    code: code,
-    store_history: false
-  });
-
-  return new Promise(async (resolve, reject) => {
-    const results: IMessage<IOPubMessageType>['content'][] = [];
-
-    future.onIOPub = (msg: IMessage<IOPubMessageType>) => {
-      const msgType = msg.header.msg_type;
-      switch (msgType) {
-        case 'execute_result':
-          results.push(msg.content);
-          break;
-        default:
-          break;
-      }
-      return;
-    };
-
-    future.onReply = msg => {
-      if (msg.content.status === 'ok') {
-        results.push(msg.content);
-      } else {
-        reject(
-          new Error('Execution failed with status: ' + msg.content.status)
-        );
-      }
-    };
-
-    // Tiemout
-    if (timeout) {
-      setTimeout(() => {
-        future.dispose();
-        reject(new Error('callKernel timeout'));
-      }, timeout);
-    }
-
-    await future.done;
-    resolve(results);
-  });
-};
+// https://github.com/jupyterlab/extension-examples/blob/main/kernel-messaging
 
 export default plugin;
